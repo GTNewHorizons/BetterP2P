67d77c6a87a6154a09fbeec34b51c2ff5286a0ca
Firenoo <49818773+firenoo@users.noreply.github.com>
2023-07-15 11:43:41 -0700

    Use CityHash64
    Should fix an insidious bug where two p2ps could have the same hash if the
    first p2p has coords (x=a,y=b,z=c,facing=F,dim=D) and the second p2p has
    coords (x=-a-1,y=b,z=c,facing=F,dim=D). This is because using XOR with a
    negative coord causes it to flip in two's complement.

--- src/main/java/com/projecturanus/betterp2p/network/HashHelper.kt
+++ src/main/java/com/projecturanus/betterp2p/network/HashHelper.kt
@@ -2,46 +2,57 @@ package com.projecturanus.betterp2p.network
 
 import appeng.parts.p2p.PartP2PTunnel
 
+
 /**
  * Generates a 64-bit hash code from X, Y, Z, Facing, and Dimension info.
- * bits 0-15: x
- * bits 16-31: y
- * bits 32-47: z
+ * bits 0-47: FashHash of (x, y, z) ->
  * bits 48-59: dim
  * bits 60-62: facing
  * bit 63: Reserved
  */
 fun hashP2P(x: Int, y: Int, z: Int, facing: Int, dim: Int): Long {
-    var ret = facing.toLong() shl 60
-    ret = ret or xorCollapse16(x).toLong()
-    ret = ret or (xorCollapse16(y).toLong() shl 16)
-    ret = ret or (xorCollapse16(z).toLong() shl 32)
-    ret = ret or (xorCollapse12(dim).toLong() shl 48)
-    return ret
+    var ret = facing.toULong() shl 61
+    val lo: ULong = x.toULong() or (y.toULong() shl 32)
+    val hi: ULong = z.toULong() or (dim.toULong() shl 32)
+    var hash = hashLen16(lo, hi)
+    hash = hash xor (hash shr 60)
+    return (ret or hash).toLong()
 }
 
 fun hashP2P(p: PartP2PTunnel<*>): Long
     = hashP2P(p.location.x, p.location.y, p.location.z, p.side.ordinal, p.location.dimension)
 
+const val k2 = 0x9ae16a3b2f90404fUL
+
 /**
- * Collapse an 32-bit integer into a 16-bit integer through
- * the XOR operator.
+ * Fetches the contiguous byte-aligned 64 bits from the 128 bit "register". Only works for indexes 1-7.
+ * lo - low 64 bits
+ * hi - high 64 bits
+ * idx - start byte index, should be 1-7. don't even bother using this method for other amounts
  */
-private fun xorCollapse16(input: Int): Int {
-    var ret = input and 0xFFFF
-    ret = ret xor ((input ushr 16) and 0xFFFF)
-    return ret
+private fun fetch64(lo: ULong, hi: ULong, idx: Int): ULong {
+    return (lo shr (idx * Byte.SIZE_BITS)) or (hi shl ULong.SIZE_BITS - (idx * Byte.SIZE_BITS))
 }
 
 /**
- * Collapse an 32-bit integer into a 12-bit integer through
- * the XOR operator.
+ * City64 hash. It's basically black magic, but here's a link
+ * https://opensource.googleblog.com/2011/04/introducing-cityhash.html
  */
-private fun xorCollapse12(input: Int): Int {
-    var ret = input and 0xFFF
-    ret = ret xor ((input ushr 12) and 0xFFF)
-    ret = ret xor ((input ushr 12) and 0xFFF)
-    return ret
+@OptIn(kotlin.ExperimentalStdlibApi::class)
+private fun hashLen16(lo: ULong, hi: ULong): ULong {
+    val mul: ULong = k2 + 32U
+    val a: ULong = lo + k2
+    val b: ULong = hi
+    val c: ULong = b.rotateRight(37) * mul + a
+    val d: ULong = (a.rotateRight(25) + b) * mul
+
+    var e: ULong = (c xor d) * mul
+    e = e xor (e shr 47)
+    var f: ULong = (d xor e) * mul
+    f = f xor (f shr 47)
+    f *= mul
+
+    return f
 }
 
 /**
