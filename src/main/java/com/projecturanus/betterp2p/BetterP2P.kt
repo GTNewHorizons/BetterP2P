61a1b3a1aa2a639e550dd5184c8b840f83145bfa
Kogepan229 <kogepan25629@gmail.com>
2026-06-16 02:14:00 +0900

    fix compile

--- src/main/java/com/projecturanus/betterp2p/BetterP2P.kt
+++ src/main/java/com/projecturanus/betterp2p/BetterP2P.kt
@@ -17,10 +17,8 @@ const val MODID = "betterp2p"
 /**
  * Better P2P is created by LasmGratel.
  * GlodBlock backported this to 1.7.10
- *
- * MODVER below is handled by gradle, ignore IDE errors.
  */
-@Mod(modid = MODID, version = MODVER, modLanguageAdapter = "net.shadowfacts.forgelin.KotlinAdapter", dependencies = "required-after:appliedenergistics2@[rv3-beta-238,); required-after:forgelin;")
+@Mod(modid = MODID, version = BuildTags.VERSION, modLanguageAdapter = "net.shadowfacts.forgelin.KotlinAdapter", dependencies = "required-after:appliedenergistics2@[rv3-beta-238,); required-after:forgelin;")
 object BetterP2P {
 
     lateinit var logger: Logger
